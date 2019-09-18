1a20266297d1d3073f4c874bedb4f43a86274d57
alizza-ops <52742283+alizza-ops@users.noreply.github.com>
2019-09-18 22:40:47 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,14 @@
+var date=18;
+var month='september';
+var year=[20,19];
+var correct=true;
+var yest=null;
+
+if (date==20) {
+	console.log(correct);
+}
+else{
+	console.log(yest);
+}
+
+console.log("today :", date,month,year[1]);
